2f5865a7ebdd7de120fa2701ae4bf5bf3b293a74
Biswajit Hemram <biswajithemram753@gmail.com>
2023-10-15 18:17:33 +0530

    storing html content in js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let clean = document.querySelector("#clean");
+    let equal = document.querySelector("#equalBtn");
+    let decmial = document.querySelector("#decimal");
+
+    let numbers = document.querySelectorAll(".number")
+    let operator = document.querySelectorAll(".opeator")
+
+    let previousScreen = document.querySelector("#lastOperationScreen");
+    let currentScreen = document.querySelector("#currentOperationScreen");
+
+})
