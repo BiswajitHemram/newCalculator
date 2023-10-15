809ec17db0e42024411d98cde17d0917b3d6d33f
Biswajit Hemram <biswajithemram753@gmail.com>
2023-10-15 18:31:50 +0530

    handleNumber() is for current number and line 1 to 3 for to store operand and operator

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
+let previewValue = "";
+let currentValue = "";
+let operator = "";
+
 document.addEventListener("DOMContentLoaded", function() {
+    // storing html components on js
     let clean = document.querySelector("#clean");
     let equal = document.querySelector("#equalBtn");
     let decmial = document.querySelector("#decimal");
 
     let numbers = document.querySelectorAll(".number")
-    let operator = document.querySelectorAll(".opeator")
+    let operators = document.querySelectorAll(".opeator")
 
     let previousScreen = document.querySelector("#lastOperationScreen");
     let currentScreen = document.querySelector("#currentOperationScreen");
 
+    numbers.forEach((numbers) => numbers.addEventListener("click", function(event){
+        if(currentValue.length <= 5){
+            handleNumber(+event.target.textContent)
+            currentScreen.textContent = currentValue;
+        }
+    }))
 })
+
+function handleNumber(num){
+    currentValue += num;
+}
